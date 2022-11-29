76a8f96216607d53d3c6f8c028ae1ffbc22d5afc
mango-324 <mashamango324@gmail.com>
2022-11-29 11:43:36 +0000

    Find roots of quadratic equation

--- main2.py
+++ main2.py
@@ -1,21 +1,25 @@
 stroka = input()
 test = stroka.replace(" ", "").replace("-", "").replace(".", "")
 if test.isdigit():
-    a, b, c = map(float, stroka.split())
-    if a == 0:
-        if b != 0:
-            print(-c / b)
-        if b == 0:
-            print(c)
+    numbers = stroka.split()
+    if len(numbers) != 3:
+        print("Введите 3 числа")
     else:
-        D = b ** 2 - (4 * a * c)
-        if D > 0 or D < 0:
-            x1 = (-b + D ** 0.5) / (2 * a)
-            x2 = (-b - D ** 0.5) / (2 * a)
-            print(x1)
-            print(x2)
-        elif D == 0:
-            x = (-b) / (2 * a)
-            print(x)
+        a, b, c = map(float, numbers)
+        if a == 0:
+            if b != 0:
+                print(-c / b)
+            if b == 0:
+                print(c)
+        else:
+            D = b ** 2 - (4 * a * c)
+            if D > 0 or D < 0:
+                x1 = (-b + D ** 0.5) / (2 * a)
+                x2 = (-b - D ** 0.5) / (2 * a)
+                print(x1)
+                print(x2)
+            elif D == 0:
+                x = (-b) / (2 * a)
+                print(x)
 else:
     print("Впишите числа")
